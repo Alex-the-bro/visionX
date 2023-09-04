12c7594c4c225a41ddd5658ad8883b21a967c1c2
Alex Mwansa <alexmwansa93@gmail.com>
2023-09-04 21:36:19 +0200

    routes updated

--- server/index.js
+++ server/index.js
@@ -10,12 +10,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173' // Replace with your local clients URL
-  }));
-
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({limit: '50mb', extended:true}));
 
 
 app.use('/api/v1/post', postRoutes);
