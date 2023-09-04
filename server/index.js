bd2e68d2b42c01fef8ae0cdd1673cbad13e1e3cd
Alex Mwansa <alexmwansa93@gmail.com>
2023-09-04 20:17:34 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './MongoDB/connect.js';
-import postRoutes from './routes/postroutes.js';
-import dalleRoutes from './routes/Dalleroutes.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
